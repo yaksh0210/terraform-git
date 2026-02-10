2dd24f29478e59bb6bab82c485f0ae0c035b5af9
Einfochips@1234 <yakshbdm02@gmail.com>
2026-02-10 15:50:39 +0530

    terraform vm changes

--- main.tf
+++ main.tf
@@ -52,8 +52,8 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
+    offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
-    version   = "latest"
+    version   = "22.04.202309120"
   }
 }
